1d597fb0ba111192b7f7d1a774fef3174dd5303f
Trevor Werner <trezrevner@icloud.com>
2023-05-07 17:11:17 -0400

    Fixed 1st 2nd row inputs mobile styles

--- app/components/SectionAOptions.jsx
+++ app/components/SectionAOptions.jsx
@@ -11,41 +11,42 @@ export default function CheckOptions({
    column
 }) {
    return (
-      <FormGroup
-         className="flex flex-row flex-nowrap items-center justify-between"
-         sx={{ flexDirection: "row" }}
-      >
-         <Typography
-            variant="body1"
-            display="flex"
-            position="absolute"
-            left={column === 2 && "50%"}
-         >
-            {option}
-         </Typography>
-         <Box>
-            <FormControlLabel
-               control={<Checkbox />}
-               label="Yes"
-               sx={{
-                  position: "relative",
-                  marginLeft: "11rem",
-                  visibility:
-                     option === "Other" &&
-                     "hidden"
-               }}
-            />
-            <FormControlLabel
-               control={<Checkbox />}
-               label="No"
-               sx={{
-                  position: "relative",
-                  visibility:
-                     option === "Other" &&
-                     "hidden"
-               }}
-            />
-         </Box>
-      </FormGroup>
+      <></>
+      // <FormGroup
+      //    className="flex flex-nowrap items-center justify-between"
+      //    sx={{ flexDirection: "row" }}
+      // >
+      //    <Typography
+      //       variant="body1"
+      //       display="flex"
+      //       position="absolute"
+      //       left={column === 2 && "50%"}
+      //    >
+      //       {option}
+      //    </Typography>
+      //    <Box>
+      //       <FormControlLabel
+      //          control={<Checkbox />}
+      //          label="Yes"
+      //          sx={{
+      //             position: "relative",
+      //             marginLeft: "11rem",
+      //             visibility:
+      //                option === "Other" &&
+      //                "hidden"
+      //          }}
+      //       />
+      //       <FormControlLabel
+      //          control={<Checkbox />}
+      //          label="No"
+      //          sx={{
+      //             position: "relative",
+      //             visibility:
+      //                option === "Other" &&
+      //                "hidden"
+      //          }}
+      //       />
+      //    </Box>
+      // </FormGroup>
    )
 }
