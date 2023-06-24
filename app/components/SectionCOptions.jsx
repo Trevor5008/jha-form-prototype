2b751ad161a1fd5d502a7a243ba16343e125c165
Trevor Werner <trezrevner@icloud.com>
2023-06-23 21:13:41 -0400

    Added content for c.2 thru c.4

--- app/components/SectionCOptions.jsx
+++ app/components/SectionCOptions.jsx
@@ -6,7 +6,12 @@ import {
    TextField
 } from "@mui/material"
 import OptionInput from "./OptionInput"
-import { situationElements } from "@/lib/options"
+import {
+   situationElements,
+   hazards,
+   hazardControls,
+   properPpe
+} from "@/lib/options"
 
 export default function SelectCOptions() {
    return (
@@ -83,7 +88,7 @@ export default function SelectCOptions() {
                helperText="* provide details above"
             />
          </Box>
-         {/* C.2 */}
+         {/* C.2 - Hazards */}
          <Typography
             variant="h3"
             marginBottom={1}
@@ -91,6 +96,187 @@ export default function SelectCOptions() {
             <span className="font-bold">C.2</span>{" "}
             Hazards
          </Typography>
+         <Box
+            sx={{
+               width: { tablet: "50%" },
+               marginLeft: {
+                  xs: 0,
+                  tablet: 2,
+                  md: 4
+               }
+            }}
+         >
+            {hazards.map((opt, idx) => {
+               return (
+                  <OptionInput
+                     key={idx}
+                     option={opt}
+                     hasLabel={true}
+                  />
+               )
+            })}
+            <TextField
+               label="Work of Others"
+               variant="standard"
+               sx={{
+                  fontSize: {
+                     xs: 14,
+                     sm: 16
+                  },
+                  "& .MuiFormLabel-root": {
+                     color: "black",
+                     fontSize: {
+                        xs: 14,
+                        sm: 16
+                     }
+                  },
+                  paddingRight: 1,
+                  width: "100%",
+                  marginBottom: 2
+               }}
+               helperText="* provide details above"
+            />
+            <TextField
+               label="Other"
+               variant="standard"
+               sx={{
+                  fontSize: {
+                     xs: 14,
+                     sm: 16
+                  },
+                  "& .MuiFormLabel-root": {
+                     color: "black",
+                     fontSize: {
+                        xs: 14,
+                        sm: 16
+                     }
+                  },
+                  paddingRight: 1,
+                  width: "100%",
+                  marginBottom: 2
+               }}
+               helperText="* provide details above"
+            />
+         </Box>
+         {/* C.3 - Controls */}
+         <Typography
+            variant="h3"
+            marginBottom={1}
+         >
+            <span className="font-bold">C.3</span>{" "}
+            Hazard Controls
+         </Typography>
+         <Box
+            sx={{
+               width: { tablet: "50%" },
+               marginLeft: {
+                  xs: 0,
+                  tablet: 2,
+                  md: 4
+               }
+            }}
+         >
+            {hazardControls.map((opt, idx) => {
+               return (
+                  <OptionInput
+                     key={idx}
+                     option={opt}
+                     hasLabel={true}
+                  />
+               )
+            })}
+            <TextField
+               label="Work of Others"
+               variant="standard"
+               sx={{
+                  fontSize: {
+                     xs: 14,
+                     sm: 16
+                  },
+                  "& .MuiFormLabel-root": {
+                     color: "black",
+                     fontSize: {
+                        xs: 14,
+                        sm: 16
+                     }
+                  },
+                  paddingRight: 1,
+                  width: "100%",
+                  marginBottom: 2
+               }}
+               helperText="* provide details above"
+            />
+            <TextField
+               label="Other"
+               variant="standard"
+               sx={{
+                  fontSize: {
+                     xs: 14,
+                     sm: 16
+                  },
+                  "& .MuiFormLabel-root": {
+                     color: "black",
+                     fontSize: {
+                        xs: 14,
+                        sm: 16
+                     }
+                  },
+                  paddingRight: 1,
+                  width: "100%",
+                  marginBottom: 2
+               }}
+               helperText="* provide details above"
+            />
+         </Box>
+         {/* C.4 - PPE */}
+         <Typography
+            variant="h3"
+            marginBottom={1}
+         >
+            <span className="font-bold">C.4</span>{" "}
+            Proper PPE
+         </Typography>
+         <Box
+            sx={{
+               width: { tablet: "50%" },
+               marginLeft: {
+                  xs: 0,
+                  tablet: 2,
+                  md: 4
+               }
+            }}
+         >
+            {properPpe.map((opt, idx) => {
+               return (
+                  <OptionInput
+                     key={idx}
+                     option={opt}
+                     hasLabel={true}
+                  />
+               )
+            })}
+            <TextField
+               label="Other"
+               variant="standard"
+               sx={{
+                  fontSize: {
+                     xs: 14,
+                     sm: 16
+                  },
+                  "& .MuiFormLabel-root": {
+                     color: "black",
+                     fontSize: {
+                        xs: 14,
+                        sm: 16
+                     }
+                  },
+                  paddingRight: 1,
+                  width: "100%",
+                  marginBottom: 2
+               }}
+               helperText="* provide details above"
+            />
+         </Box>
       </Container>
    )
 }
