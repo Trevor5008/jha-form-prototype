923d881abec2b6fc120d2851777a3ac0fffe7182
Trevor Werner <trezrevner@icloud.com>
2023-07-09 19:04:38 -0400

    Added feature slices for all c sections along with add and remove functionality

--- app/(pages)/page-four/page.jsx
+++ app/(pages)/page-four/page.jsx
@@ -2,23 +2,69 @@
 import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 import Link from "next/link"
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from "react-redux"
+import { selectSituation } from "@/app/features/situations/situationsSlice"
 import { selectHazards } from "@/app/features/hazards/hazardsSlice"
+import { selectControls } from "@/app/features/hazard-controls/hazardControlsSlice"
+import { selectPpe } from "@/app/features/ppe/ppeSlice"
 
 export default function PageFour() {
-    const hazards = useSelector(selectHazards)
-      console.log(hazards)
+   const hazards = useSelector(selectHazards)
+   const controls = useSelector(selectControls)
+   const ppe = useSelector(selectPpe)
+   const situation = useSelector(selectSituation)
+
    return (
       <Container>
          <Box>
-            <ul>
-               {hazards.map(hazard => {
-                  return (
-                     <li>{hazard}</li>
-                  )
-               })}
-            </ul>
+            <Typography variant="h2">
+               Situtation/Task
+            </Typography>
+            <Box marginLeft={1}>
+               <ul>
+                  {situation.map((task) => {
+                     return <li>{task}</li>
+                  })}
+               </ul>
+            </Box>
+         </Box>
+         <Box>
+            <Typography variant="h2">
+               Hazards
+            </Typography>
+            <Box marginLeft={1}>
+               <ul>
+                  {hazards.map((hazard) => {
+                     return <li>{hazard}</li>
+                  })}
+               </ul>
+            </Box>
+         </Box>
+         <Box>
+            <Typography variant="h2">
+               Hazard Controls
+            </Typography>
+            <Box marginLeft={1}>
+               <ul>
+                  {controls.map((control) => {
+                     return <li>{control}</li>
+                  })}
+               </ul>
+            </Box>
+         </Box>
+         <Box>
+            <Typography variant="h2">
+               PPE
+            </Typography>
+            <Box marginLeft={1}>
+               <ul>
+                  {ppe.map((item) => {
+                     return <li>{item}</li>
+                  })}
+               </ul>
+            </Box>
          </Box>
          <Box
             display="flex"
--- app/store.js
+++ app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import hazardsReducer from './features/hazards/hazardsSlice'
+import situationsReducer from './features/situations/situationsSlice'
+import hazardControlsReducer from './features/hazard-controls/hazardControlsSlice'
+import ppeReducer from './features/ppe/ppeSlice'
 
 export default configureStore({
     reducer: {
-        hazards: hazardsReducer
+        situations: situationsReducer,
+        hazards: hazardsReducer,
+        controls: hazardControlsReducer,
+        ppe: ppeReducer
     }
 })
